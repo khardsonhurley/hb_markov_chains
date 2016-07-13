060432445f27d6aa620e57d0c7dfd6848d955995
Hackbright Student <info@hackbrightacademy.com>
2016-07-12 17:40:44 -0700

    Completed all functions and bonus formatting function for green-eggs.txt

--- markov.py
+++ markov.py
@@ -7,9 +7,13 @@ def open_and_read_file(file_path):
     Takes a string that is a file path, opens the file, and turns
     the file's contents as one string of text.
     """
-    text_string = open(file_path) #open file and save as a variable
-    text_string = text_string.read() #save content as a string
-    return text_string #returns a string of the content 
+
+    #open file and save as a variable
+    text_string = open(file_path) 
+    #save content as a string
+    text_string = text_string.read() 
+    #returns a string of the content 
+    return text_string 
 
 
 def make_chains(text_string):
@@ -24,50 +28,82 @@ def make_chains(text_string):
         >>> make_chains("hi there mary hi there juanita")
         {('hi', 'there'): ['mary', 'juanita'], ('there', 'mary'): ['hi'], ('mary', 'hi': ['there']}
     """
-    words = text_string.split() #takes in string and provides a list of all the words
-    chains = {} #create empty dictionary that will contain word pairings 
-                #with their third word options
 
-    for i in range(len(words)-2):   #iterate using the indices of the list
-        word_tuple = tuple([words[i], words[i + 1]])    #get first key, turn into tuple, save 
-                                                        #as variable
-        chains[word_tuple] = []     #add key to dictionary, if doesn't exist default empty 
-                                    #list as value
-    for i in range(len(words)-2):   #iterate using the indices of the list
-        word_tuple = tuple([words[i], words[i + 1]]) #get first key, turn into tuple, save as 
-                                                     #variable
-        chains[word_tuple] = chains[word_tuple] + [words[i+2]] #Adding new word (in list) to 
-                                                                #old list in the value of key 
-                                                                #word_tuple
+    #takes in string and provides a list of all the words
+    words = text_string.split() 
+    #create empty dictionary that will contain word pairings with their third word options
+    chains = {} 
+
+    #iterate using the indices of the list
+    for i in range(len(words)-2):
+        #get first key, turn into tuple, save as variable
+        word_tuple = tuple([words[i], words[i + 1]])     
+        
+        #add key to dictionary, if doesn't exist default empty list as value  
+        values_list = chains.get(word_tuple, [])
+
+        #Adding new word (in list) to old list in the value of key word_tuple
+        values_list.append(words[i+2])
+
+        chains[word_tuple] = values_list
+
     return chains
 
 
 def make_text(chains):
-    """Takes dictionary of markov chains; returns random text."""
+    """Takes dictionary of markov chains; returns random text as a string."""
 
-    #text = "" #string to be used at very end
+    #string to be used at very end
+    text = " " 
 
-    sentence = [] #create empty list "sentence"
-    dict_of_words = chains #saves the inputted dictionary as a variable
+    #create empty list "sentence"
+    sentence = [] 
+    #saves the inputted dictionary as a variable
+    dict_of_words = chains 
 
-    first_tuple = choice(dict_of_words.keys())   #randomly selected a key in the 
-                                                        #dictionary
+    #randomly selected a key in the dictionary
+    first_tuple = choice(dict_of_words.keys())
+
+    #add tuple[0] to our sentence
+    sentence.append(first_tuple[0])
+    #add tuple[1] to our sentence
+    sentence.append(first_tuple[1])
     
-    #VERIFY THE TUPLE: If tuple not in dictionary, end loop and print text. 
-
-    sentence.append(first_tuple[0])#add tuple[0] to our sentence
-    sentence.append(first_tuple[1])#add tuple[1] to our sentence
-    print sentence
-    random_third_word = choice(dict_of_words[first_tuple])
-    print random_third_word
     #randomly pick an item in the value's list
+    random_third_word = choice(dict_of_words[first_tuple])   
+
     #add the new string to "sentence"
-    #to get new key call index -2 and -1 of "sentence"--> creating a tuple
-    #call dictionary on new tuple --> unpack etc. 
+    sentence.append(random_third_word) 
+    
 
-    #turn "sentence" into long string by adding elements into the string "text"
+    while True:
+        #Grabs last two items of sentence and creating new tuple to become our key
+        end_tuple = tuple([sentence[-2],sentence[-1]]) 
 
+        #If the key is not in our dictionary, the loop breaks.
+        if end_tuple not in dict_of_words: 
+            break
+        #Otherwise, we randomly select another value from the list of values of 
+        #the key (random_tuple)
+        else:
+            #randomly pick an item in the value's list                               
+            third_word = choice(dict_of_words[end_tuple]) 
+            #Add the third_word to the sentence list. 
+            sentence.append(third_word)   
 
+    #joining the sentence list together to create one string
+    return text.join(sentence) 
+
+def format_string(string):
+    text= ""
+    string_list = string.split("?")
+    for i in range(len(string_list)-1):
+        string_list[i] = string_list[i] + "?\n"
+    
+    string_list[0] = " "+string_list[0]
+
+    return text.join(string_list)
+        
 
 input_path = "green-eggs.txt"
 
@@ -80,5 +116,5 @@ chains = make_chains(input_text)
 # # Produce random text
 random_text = make_text(chains)
 
-random_text
+print random_text
 
